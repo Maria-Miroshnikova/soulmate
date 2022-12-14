b9e0b8fd178df8f6bc3ba066493149860be61b0e
Mariya Miroshnikova <mari.mirosh@bk.ru>
2022-12-14 11:12:56 +0300

    make friend button TELEGRAM linkable

--- Frontend/src/components/UI/buttons/ButtonFriend.tsx
+++ Frontend/src/components/UI/buttons/ButtonFriend.tsx
@@ -2,7 +2,8 @@ import React, {FC} from 'react';
 import {useAppSelector} from "../../../hooks/redux";
 import {userdataAPI} from "../../../services/userdataService";
 import {UserPersonalInfoModel} from "../../../types/UserModels";
-import {Box, Button} from "@mui/material";
+import {Box, Button, Link} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 export interface ButtonPersonProps {
     person: UserPersonalInfoModel
@@ -11,6 +12,7 @@ export interface ButtonPersonProps {
 const ButtonFriend: FC<ButtonPersonProps> = ({person}) => {
     const textDelete = "Удалить";
     const textTelegram = "Telegram";
+    const navigate = useNavigate();
 
     const userId = useAppSelector(state => state.authReducer.userId);
     const [removeFriend] = userdataAPI.useRemovePersonFromFriendsMutation()
@@ -19,15 +21,10 @@ const ButtonFriend: FC<ButtonPersonProps> = ({person}) => {
         removeFriend({personId: person.id, userId: userId!});
     }
 
-    // TODO: open telegram
-    const handleTelegram = () => {
-
-    }
-
     return (
         <Box display="flex" flexDirection="row" gap={1}>
             <Button variant="contained" onClick={handleDelete} > {textDelete} </Button>
-            <Button variant="contained" onClick={handleTelegram} > {textTelegram} </Button>
+            <Button variant="contained" href={person.telegram}> {textTelegram} </Button>
         </Box>
     );
 };
